62512d57838da4bc604277f82f41b684a8b96705
seamussmith <student.141504@worcesterschools.net>
2021-11-04 08:37:38 -0400

    god

--- src/main/App.java
+++ src/main/App.java
@@ -1,17 +1,22 @@
 package main;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.IntPredicate;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class App
 {
     public static void main(String[] args) 
     {
-        var x = System.currentTimeMillis();
-        System.out.println(primeSieve(1000));
-        System.out.println(System.currentTimeMillis() - x);
+        // var x = System.currentTimeMillis();
+        // System.out.println(primeSieve(1000));
+        // System.out.println(System.currentTimeMillis() - x);
+        var y = System.currentTimeMillis();
+        printFiles("./TestFiles", "a");
+        System.out.println(System.currentTimeMillis() - y);
     }
     public static int primeSieve(int upto)
     {
@@ -21,5 +26,30 @@ public class App
                 .filter(p -> IntStream.range(2, (int)Math.sqrt(p)).allMatch(x -> p % x != 0))
                 .limit(upto)
                 .sum();
+    }
+    public static void printFiles(String directory, String match)
+    {
+        try
+        {
+            var matchingLines = Files.list(Paths.get(directory))
+                    .parallel()
+                    .map(x -> x.toFile())
+                    .filter(x -> x.isFile())
+                    .map(x -> { // RIP Beautiful functional syntax
+                        try
+                        {
+                            return Files.readAllLines(x.toPath());
+                        }
+                        catch (IOException e) { throw new RuntimeException(e); }
+                    })
+                    .mapToInt(x -> x.stream()
+                            .parallel()
+                            .mapToInt(y -> y.toLowerCase()
+                                    .contains(match) ? 1 : 0)
+                            .sum())
+                    .sum();
+            System.out.println(matchingLines);
+        }
+        catch (IOException e) { throw new RuntimeException(e); }
     }
 }
