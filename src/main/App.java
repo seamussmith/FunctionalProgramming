378af7aed6e106022d90f156a03b7d30314e368e
seamussmith <student.141504@worcesterschools.net>
2021-11-04 07:13:32 -0400

    Prime sieve

--- src/main/App.java
+++ src/main/App.java
@@ -1,9 +1,25 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
 public class App
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello, World!");
+        var x = System.currentTimeMillis();
+        System.out.println(primeSieve(1000));
+        System.out.println(System.currentTimeMillis() - x);
+    }
+    public static int primeSieve(int upto)
+    {
+        return IntStream.iterate(2, x -> x + 1)
+                .parallel()
+                .filter(x -> x == 2 || x % 2 != 0)
+                .filter(p -> IntStream.range(2, (int)Math.sqrt(p)).allMatch(x -> p % x != 0))
+                .limit(upto)
+                .sum();
     }
 }
