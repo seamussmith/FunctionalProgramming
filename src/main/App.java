e3191f8fe4411b15336656eccebdae4a6d0e3e91
seamussmith <student.141504@worcesterschools.net>
2021-11-05 08:02:02 -0400

    remove unused imports

--- src/main/App.java
+++ src/main/App.java
@@ -1,10 +1,8 @@
 package main;
+
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class App
